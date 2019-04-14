1b9fc3f43b205ab47da6d4f89130e653764c31e8
paulofr17 <paulofr17@gmail.com>
2019-04-14 19:32:50 +0100

    Ficha7(update)

--- 2ºano/SO/07/f7.c
+++ 2ºano/SO/07/f7.c
@@ -5,27 +5,32 @@
 #include <stdio.h>
 #include <string.h>
 
+//signal(sinal,f) substitui o sinal definido por uma função f que defenimos
+//os sinais interrompem tudo que está a acontecer e voltam ao local da execução onde o sinal foi chamado
+//O SIGKILL/SIGSTOP/SIGCONT não podem ser alterados por funções para segurança do sistema operativo
+
 int n = 0;
 int seconds = 0;
 
 
 void printSIGQUIT(){
-	char str[10];
-	sprintf(str,"%d\n",n);
-	write(1,str,sizeof(str));
+	char str[30];
+	sprintf(str," Número de ctrl+c: %d\n",n);
+	write(1,str,strlen(str));
+	_exit(0);
 }
 
 void printSIGINT(){
 	n++;
-	char str[15];
-	sprintf(str,"Time: %ds\n",seconds);
-	write(1,str,strlen(str)*sizeof(char));
+	char str[30];
+	sprintf(str," Time: %ds\n",seconds);
+	write(1,str,strlen(str));
 }
 
 void printTSTP(){
-	char str[20];
+	char str[30];
 	sprintf(str,"Número de ctrl+c: %d\n",n);
-	write(1,str,strlen(str)*sizeof(char));
+	write(1,str,strlen(str));
 	_exit(0);
 }
 
@@ -38,9 +43,9 @@ void ex1(){
 	alarm(1);
 	signal(SIGALRM,mysleep);
 	signal(SIGINT,printSIGINT);
-	signal(SIGTSTP,printTSTP);
-	//signal(SIGQUIT,printSIGQUIT);
-	while(1) pause();
+	signal(SIGQUIT,printSIGQUIT);
+	//signal(SIGTSTP,printTSTP);
+	while(1) pause(); //coloca um processo em pausa até receber um sinal
 }
 
 void main(int argc, char** argv){
